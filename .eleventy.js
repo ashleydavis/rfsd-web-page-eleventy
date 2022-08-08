1fc877eca38fd7d07ad7c5c42c05af898029d259
Ashley Davis <ashley@codecapers.com.au>
2022-08-08 17:25:07 +1000

    Now generating images to "figure" elements and wrote custom JS/CSS to make the figures "zoomable".

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,20 @@
+const md = require('markdown-it');
+const implicitFigures = require('markdown-it-image-figures');
+
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('src/assets');
+
+    let options = {
+        html: true,
+        breaks: true,
+        linkify: true
+    };
+    
+    const m = md(options);
+    m.use(implicitFigures);
+
+    eleventyConfig.setLibrary("md", m);
 
     return {
         dir: {
